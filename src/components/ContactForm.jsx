a92be41323ad3d5d2cd6f788f3e0a7964d953136
Seby <lokuraa18@hotmail.com>
2026-03-02 13:35:12 -0300

    new sections add

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -6,11 +6,12 @@ export default function ContactForm() {
     name: "",
     email: "",
     number: "",
-    message: ""
+    message: "",
   });
 
   const [loading, setLoading] = useState(false);
-  const [success, setSuccess] = useState("");
+  const [feedback, setFeedback] = useState("");
+  const [feedbackType, setFeedbackType] = useState(""); // "success" | "error"
 
   // Actualiza el estado cada vez que escribimos en un input
   const handleChange = (e) => {
@@ -19,31 +20,78 @@ export default function ContactForm() {
 
   // Se ejecuta cuando el usuario envía el formulario
   const handleSubmit = async (e) => {
+    setFeedback("");
+    setFeedbackType("");
+
     e.preventDefault(); // evita que la página recargue
-    setLoading(true);   // activa un estado de "loading"
+    setLoading(true); // activa un estado de "loading"
 
     try {
       const data = await sendContact(form); // llama a la función que hace POST al backend
-      setSuccess(data.message);             // mostramos mensaje de éxito
+      setFeedback(data.message);
+      setFeedbackType("success"); // mostramos mensaje de éxito
       setForm({ name: "", email: "", number: "", message: "" }); // resetea formulario
     } catch (err) {
       console.error(err);
-      setSuccess(err.response?.data?.message || "Error sending message"); // manejo de error
+      setFeedback(err.response?.data?.message || "Error sending message"); // manejo de error
+      //setFeedbackType("error");
     } finally {
       setLoading(false); // desactiva loading
     }
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-4 p-4 bg-base-200 rounded-lg shadow-lg">
-      <input type="text" name="name" placeholder="Name" value={form.name} onChange={handleChange} className="input input-bordered w-full" />
-      <input type="email" name="email" placeholder="Email" value={form.email} onChange={handleChange} className="input input-bordered w-full" />
-      <input type="tel" name="number" placeholder="Phone number" value={form.number} onChange={handleChange} className="input input-bordered w-full" />
-      <textarea name="message" placeholder="Message" value={form.message} onChange={handleChange} className="textarea textarea-bordered w-full"></textarea>
-      <button type="submit" className="btn btn-neutral btn-outline" disabled={loading}>
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-4 p-4 bg-base-200 rounded-lg shadow-lg"
+    >
+      <input
+        type="text"
+        name="name"
+        placeholder="Name"
+        value={form.name}
+        onChange={handleChange}
+        className="input input-bordered w-full"
+      />
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={form.email}
+        onChange={handleChange}
+        className="input input-bordered w-full"
+      />
+      <input
+        type="tel"
+        name="number"
+        placeholder="Phone number"
+        value={form.number}
+        onChange={handleChange}
+        className="input input-bordered w-full"
+      />
+      <textarea
+        name="message"
+        placeholder="Message"
+        value={form.message}
+        onChange={handleChange}
+        className="textarea textarea-bordered w-full"
+      ></textarea>
+      <button
+        type="submit"
+        className="btn btn-neutral btn-outline"
+        disabled={loading}
+      >
         {loading ? "Sending..." : "Send"}
       </button>
-      {success && <p className="text-green-500">{success}</p>}
+      {feedback && (
+        <p
+          className={
+            feedbackType === "success" ? "text-green-500" : "text-red-500"
+          }
+        >
+          {feedback}
+        </p>
+      )}
     </form>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,12 @@
-import tech  from "../assets/tech.jpg"
+import tech from "../assets/tech.jpg";
 export default function Home() {
   return (
     <div className="min-h-screen relative">
-
       {/* Fondo */}
       <div
         className="absolute inset-0 bg-cover bg-center"
         style={{
-          backgroundImage:
-          `url(${tech})`,
+          backgroundImage: `url(${tech})`,
         }}
       />
 
@@ -17,7 +15,6 @@ export default function Home() {
 
       {/* Contenido encima del fondo */}
       <div className="relative z-10 text-white">
-        
         {/* HERO SECTION */}
         <section className="hero min-h-[70vh]">
           <div className="hero-content text-center">
@@ -39,28 +36,147 @@ export default function Home() {
         {/* FEATURES SECTION */}
         <section className="py-20 px-6">
           <div className="max-w-6xl mx-auto grid md:grid-cols-3 gap-10">
-
             <div className="card bg-base-200 shadow-lg">
               <div className="card-body">
                 <h2 className="card-title">⚡ Backend sólido</h2>
-                <p>API REST con Express, conexión a base de datos y manejo de errores profesional.</p>
+                <p>
+                  API REST con Express, conexión a base de datos y manejo de
+                  errores profesional.
+                </p>
               </div>
             </div>
 
-            <div className="card bg-base-200 shadow-lg">
+            <div className="card bg-base-200 shadow-lg ">
               <div className="card-body">
                 <h2 className="card-title">🎨 Diseño moderno</h2>
-                <p>Interfaz responsive usando TailwindCSS y DaisyUI con soporte para temas.</p>
+                <p>
+                  Interfaz responsive usando TailwindCSS y DaisyUI con soporte
+                  para temas.
+                </p>
               </div>
             </div>
 
             <div className="card bg-base-200 shadow-lg">
               <div className="card-body">
                 <h2 className="card-title">🔐 Arquitectura escalable</h2>
-                <p>Estructura modular lista para crecer hacia un proyecto real de producción.</p>
+                <p>
+                  Estructura modular lista para crecer hacia un proyecto real de
+                  producción.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        {/*SKILLS  */}
+
+        <section>
+          <div className="carousel w-full">
+            <div id="item1" className="carousel-item w-full relative">
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
+                className="w-full"
+              />
+
+              {/* Overlay para legibilidad */}
+              <div className="absolute inset-0 bg-black/40" />
+
+              {/* Texto encima */}
+              <div className="absolute bottom-6 left-6 right-6 text-white">
+                <h3 className="text-2xl font-bold">Cristo Vive</h3>
+                <p className="text-sm md:text-base opacity-90">
+                  Nuestro Señor y Salvador.
+                </p>
               </div>
             </div>
 
+            <div id="item2" className="carousel-item w-full relative">
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
+                className="w-full"
+              />
+              {/* Overlay para legibilidad */}
+              <div className="absolute inset-0 bg-black/40" />
+
+              {/* Texto encima */}
+              <div className="absolute bottom-6 left-6 right-6 text-white">
+                <h3 className="text-2xl font-bold">Reinara por siempre</h3>
+                <p className="text-sm md:text-base opacity-90">
+                  Por los siglos de los siglos.
+                </p>
+              </div>
+            </div>
+            <div id="item3" className="carousel-item w-full relative">
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
+                className="w-full"
+              />
+
+              {/* Overlay para legibilidad */}
+              <div className="absolute inset-0 bg-black/40" />
+
+              {/* Texto encima */}
+              <div className="absolute bottom-6 left-6 right-6 text-white">
+                <h3 className="text-2xl font-bold">Unico y verdadero Dios</h3>
+                <p className="text-sm md:text-base opacity-90">
+                  Cristo Jesus mi plenitud.
+                </p>
+              </div>
+            </div>
+            <div id="item4" className="carousel-item w-full relative">
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+                className="w-full"
+              />
+
+              {/* Overlay para legibilidad */}
+              <div className="absolute inset-0 bg-black/40" />
+
+              {/* Texto encima */}
+              <div className="absolute bottom-6 left-6 right-6 text-white">
+                <h3 className="text-2xl font-bold">TE AMO DIOS</h3>
+                <p className="text-sm md:text-base opacity-90">En vos confio</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex w-full justify-center gap-2 py-2">
+            <a href="#item1" className="btn btn-xs">
+              1
+            </a>
+            <a href="#item2" className="btn btn-xs">
+              2
+            </a>
+            <a href="#item3" className="btn btn-xs">
+              3
+            </a>
+            <a href="#item4" className="btn btn-xs">
+              4
+            </a>
+          </div>
+        </section>
+
+        {/*3d   SECTION*/}
+        <section className="py-16">
+          <div className="w-full flex justify-center">
+            <div className="hover-3d mx-auto">
+              <figure className="w-[320px] md:w-[420px] rounded-2xl overflow-hidden">
+                <img
+                  src="https://img.daisyui.com/images/stock/creditcard.webp"
+                  alt="3D card"
+                  className="w-full h-auto block"
+                />
+              </figure>
+
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
           </div>
         </section>
 
@@ -73,7 +189,6 @@ export default function Home() {
             Contactanos
           </a>
         </section>
-
       </div>
     </div>
   );
